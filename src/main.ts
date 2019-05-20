936fbcb7517fcdd2ea786c3cca9db7f1137240f7
thegrep01 <den.belotserkovets@gmail.com>
2019-05-20 21:51:49 +0300

    deps

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  // tslint:disable-next-line: no-any
-  const app: any = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    // tslint:disable-next-line: no-any
+    const app: any = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
